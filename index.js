fe2bf7d30e5c3615735df681873f3d23e3aed400
Felix Lindh <felix.lindh@student.kyh.se>
2022-12-12 09:45:44 +0100

    merged html and javascript to dev and added some css

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-/* Listeners and setting player names */
-const players = [
+
+players = [
     {playerName: "playerOne",
      symbol: "0"},
      {playerName: "playerTwo",
      symbol: "x"}
-] 
-let gameturn = 0;
-const cards = document.querySelectorAll(".card")
+];
+gameturn = 0;
+cards = document.querySelectorAll(".card")
 
 cards.forEach(card => {
     card.addEventListener("click", addSymbols);
 });
 
 function addSymbols(event) {
+    if (event.target.textContent != "") {
+        return;
+    }
     event.target.textContent = players[gameturn].symbol;
-    gameturn = gameturn + 1 % 2;
+    gameturn = (gameturn + 1) % 2;
 }
