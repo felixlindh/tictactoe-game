a52dd68c00de2d0a6780e25fc6722e1cd129ff73
Felix Lindh <felix.lindh@student.kyh.se>
2022-12-12 09:17:07 +0100

    added symbol click function to cards

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 /* Listeners and setting player names */
+const players = [
+    {playerName: "playerOne",
+     symbol: "0"},
+     {playerName: "playerTwo",
+     symbol: "x"}
+] 
+let gameturn = 0;
+const cards = document.querySelectorAll(".card")
+
+cards.forEach(card => {
+    card.addEventListener("click", addSymbols);
+});
+
+function addSymbols(event) {
+    event.target.textContent = players[gameturn].symbol;
+    gameturn = gameturn + 1 % 2;
+}
