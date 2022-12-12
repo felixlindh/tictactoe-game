6170d49ee9b3e61c15a1d57c8a674bc91d689b19
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-12 13:15:49 +0100

    synchronizing players game board

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const playerOneNameInput = document.querySelector("#player1");
 const playerTwoNameInput = document.querySelector("#player2");
 const gameBoard = document.querySelector(".gameboard");
-const enterGameBtn = document.querySelector("button");
+const enterGameBtn = document.querySelector(".start-game-btn");
 const playerTurn = document.querySelector("#playerTurn");
 const playerInputContainer = document.querySelector(".players");
 const playerOneScore = document.querySelector(".player1-score");
@@ -33,54 +33,90 @@ cards.forEach((card) => {
 });
 
 function addSymbols(event) {
-    if (event.target.innerHTML != "") {
-        return;
-    }
-    event.target.innerHTML = `<p>${players[gameturn].symbol}</p>`;
-    checkWinCondition();
-    gameturn = (gameturn + 1) % 2;
+  if (event.target.innerHTML != "") {
+    return;
+  }
+  event.target.innerHTML = `<p>${players[gameturn].symbol}</p>`;
+  checkWinCondition();
+  gameturn = (gameturn + 1) % 2;
   playerTurn.textContent = players[gameturn].playerName;
-  updateRoom(idValue, ["-", "O", "X", "X", "-", "-", "-", "-", "-"]);
-  
+  updateRoom(idValue, gameboardToArray());
 }
-const winArray = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+const winArray = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 function checkWinCondition() {
-    for (let i = 0; i < winArray.length; i++) {
-        const numbers = winArray[i];
-        if (cards[numbers[0]].innerHTML != "" && cards[numbers[1]].innerHTML != "" && cards[numbers[2]].innerHTML != "") {
-            if (cards[numbers[0]].innerHTML == cards[numbers[1]].innerHTML && cards[numbers[1]].innerHTML == cards[numbers[2]].innerHTML) {
-            players[gameturn].score++;
-            updateDisplay();
-            setTimeout(resetGame, 1200)
-        }
-        }
-        
+  for (let i = 0; i < winArray.length; i++) {
+    const numbers = winArray[i];
+    if (
+      cards[numbers[0]].innerHTML != "" &&
+      cards[numbers[1]].innerHTML != "" &&
+      cards[numbers[2]].innerHTML != ""
+    ) {
+      if (
+        cards[numbers[0]].innerHTML == cards[numbers[1]].innerHTML &&
+        cards[numbers[1]].innerHTML == cards[numbers[2]].innerHTML
+      ) {
+        players[gameturn].score++;
+        updateDisplay();
+        setTimeout(resetGame, 1200);
+      }
     }
+  }
 }
 
 function updateDisplay() {
-    playerOneScore.innerHTML = `${players[0].playerName} score: ${players[0].score}`;
-    playerTwoScore.innerHTML = `${players[1].playerName} score: ${players[1].score}`;
+  playerOneScore.innerHTML = `${players[0].playerName} score: ${players[0].score}`;
+  playerTwoScore.innerHTML = `${players[1].playerName} score: ${players[1].score}`;
+}
+
+function gameboardToArray() {
+  const board = [];
+  for (let card of cards) {
+    const p = card.querySelector("p");
+    if (p == null) {
+      board.push("-");
+    } else {
+      board.push(p.textContent);
+    }
+  }
+  return board;
 }
 
 function resetGame() {
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].innerHTML = "";
-    }
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].innerHTML = "";
+  }
 }
-console.log(cards)
+console.log(cards);
 
 let idValue;
 
-createRoom(handleRoomCreated)
+createRoom(handleRoomCreated);
 
 function handleRoomCreated(data) {
-    onRoomUpdate(data.id, handleRoomUpdate)
-    console.log(data)
-    idValue = data.id;
+  onRoomUpdate(data.id, handleRoomUpdate);
+  console.log(data);
+  idValue = data.id;
 }
 
 function handleRoomUpdate(data) {
-    console.log(data)
+  updateBoardFromData(data.board);
 }
 
+function updateBoardFromData(board) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] == "-") {
+      cards[i].innnerHTML = "";
+    } else {
+      cards[i].innnerHTML = `<p>${board[i]}</p>`;
+    }
+  }
+}
