a1eb2c00b77f8f279ac273dac488dbb3c9a2e1f5
Kettler-gamer <filip.blomqvist.fkb@hotmail.com>
2022-12-12 10:17:57 +0100

    players can set their names

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
+const playerOneNameInput = document.querySelector("#player1");
+const playerTwoNameInput = document.querySelector("#player2");
+const gameBoard = document.querySelector(".gameboard");
+const enterGameBtn = document.querySelector("button");
+const playerTurn = document.querySelector("#playerTurn");
+const playerInputContainer = document.querySelector(".players");
 
-players = [
-    {playerName: "playerOne",
-     symbol: "0"},
-     {playerName: "playerTwo",
-     symbol: "x"}
+gameBoard.style.display = "none";
+
+enterGameBtn.addEventListener("click", () => {
+  players[0].playerName = playerOneNameInput.value;
+  players[1].playerName = playerTwoNameInput.value;
+  gameBoard.style.display = "flex";
+  playerTurn.textContent = players[gameturn].playerName;
+  playerInputContainer.style.display = "none";
+});
+
+const players = [
+  { playerName: "playerOne", symbol: "0" },
+  { playerName: "playerTwo", symbol: "x" },
 ];
-gameturn = 0;
-cards = document.querySelectorAll(".card")
+let gameturn = 0;
+const cards = document.querySelectorAll(".card");
 
-cards.forEach(card => {
-    card.addEventListener("click", addSymbols);
+cards.forEach((card) => {
+  card.addEventListener("click", addSymbols);
 });
 
 function addSymbols(event) {
-    if (event.target.textContent != "") {
-        return;
-    }
-    event.target.textContent = players[gameturn].symbol;
-    gameturn = (gameturn + 1) % 2;
+  if (event.target.textContent != "") {
+    return;
+  }
+  event.target.textContent = players[gameturn].symbol;
+  gameturn = (gameturn + 1) % 2;
+  playerTurn.textContent = players[gameturn].playerName;
 }
